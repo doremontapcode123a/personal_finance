3f1303f87cf52bfa01d7e1fcb60653736a789d68
Le Minh Tan <tanle782005@gmail.com>
2025-11-14 20:23:16 +0700

    update

---
+++ app/src/main/java/com/nhom3/personalfinance/ui/auth/WelcomeActivity.java
@@ -0,0 +1,33 @@
+package com.nhom3.personalfinance.ui.auth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.nhom3.personalfinance.R;
+
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Thiết lập layout bạn đã cung cấp
+        setContentView(R.layout.activity_welcome);
+
+        Button loginButton = findViewById(R.id.btnLoginMain);
+        Button signupButton = findViewById(R.id.btnSignupMain);
+
+        // --- Chuyển hướng đến LoginActivity ---
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        // --- Chuyển hướng đến RegisterActivity ---
+        signupButton.setOnClickListener(v -> {
+            Intent intent = new Intent(WelcomeActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+    }
+}
